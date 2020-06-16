a08fac58cb18b1457e8012a50ce04273918906d0
Somaia <somaia.m.amin@gmail.com>
2020-06-16 17:23:22 +0800

    removed prints

--- mbox_parser.py
+++ mbox_parser.py
@@ -23,20 +23,16 @@ def get_contents(email):
         return email.get_payload()
 
 if __name__ == "__main__":
-    print("before file)")
     # get mbox file
-    mbox_file = "/home/somaiaamin/rachel/Pipeline-01_Lead.mbox"
-    print("after file)")
+    mbox_file = "example.mbox" #change this to your file name
     # create CSV file
     writer = csv.writer(open(export_file_name, "wb"), encoding='utf-8')
-    print("csv)")
 
     # create header row
     writer.writerow(["subject", "from", "date", "body"])
 
     # add rows based on mbox file
     for email in mailbox.mbox(mbox_file):
-        print("(writing line)")
         contents = get_contents(email)
         contents = html2text.html2text(contents)
         writer.writerow([email["subject"], email["from"], email["date"], contents])
